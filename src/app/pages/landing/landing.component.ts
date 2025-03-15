6de3e7f1f4187f10c6d7bb1d9c27d3a388c76593
EdgarQuinonez <l22410043@itparral.edu.mx>
2025-03-15 13:26:10 -0600

    moved landing rfc button inside card.

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CurpComponent } from '@core/curp/curp.component';
 import { TopbarComponent } from '@shared/components/topbar/topbar.component';
 import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-landing',
-  imports: [ButtonModule, CurpComponent, TopbarComponent],
+  imports: [ButtonModule, CurpComponent, TopbarComponent, CardModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
 })
