47d448dea841f397b066a2b6fd57cc80c87f5882
EdgarQuinonez <l22410043@itparral.edu.mx>
2025-03-16 11:58:51 -0600

    added SelectButton to the RFC form.

--- src/app/core/rfc-form/rfc-form.component.ts
+++ src/app/core/rfc-form/rfc-form.component.ts
@@ -8,6 +8,8 @@ import {
 } from 'primeng/inputgroupaddon';
 import { InputText, InputTextModule } from 'primeng/inputtext';
 import { TabPanel, TabsModule } from 'primeng/tabs';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { SelectButtonModule } from 'primeng/selectbutton';
 
 @Component({
   selector: 'app-rfc-form',
@@ -18,14 +20,33 @@ import { TabPanel, TabsModule } from 'primeng/tabs';
     InputTextModule,
     InputGroupModule,
     InputGroupAddonModule,
+    RadioButtonModule,
+    SelectButtonModule,
   ],
   templateUrl: './rfc-form.component.html',
   styleUrl: './rfc-form.component.scss',
 })
 export class RfcFormComponent {
+  tipoSujetoForm = new FormGroup({
+    tipoSujeto: new FormControl(''),
+  });
+
   rfcForm = new FormGroup({
     rfc: new FormControl(''),
   });
+
+  tipoSujetoOptions = [
+    {
+      name: 'Persona Física',
+      code: 'PF',
+      icon: 'pi pi-user',
+    },
+    {
+      name: 'Persona Moral',
+      code: 'PM',
+      icon: 'pi pi-users',
+    },
+  ];
 
   loading = false;
 
